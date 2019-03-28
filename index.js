8e38423d7197b15aae7c4a2d9d8d2851b457a9cc
LucasZibell <lucas.zibell@wolox.com.ar>
2019-03-28 17:26:32 -0300

    solve code not compiling

--- index.js
+++ index.js
@@ -1,24 +1,24 @@
 const findInFiles = require('find-in-files');
 let amountOfJs;
 
-findInFiles.find('', './test', '.js$').then(results {
-	amountOfJs = Object.keys(results).length;
-});
+findInFiles.find('', './test', '.js$').then(results => 
+	amountOfJs = Object.keys(results).length
+);
 
-findInFiles.find("from 'i18next*';", './test', '.js$').then(results => {
+findInFiles.find("from 'i18next*';", './test', '.js$').then(results => 
 	console.log(
 		'Porcentaje de uso de internacionalización del total: ',
 		(Object.keys(results).length / amountOfJs * 100).toFixed(2)
-	);
+	)
+);
 
-	findInFiles.find('=> {', './test', '.js$').then(results => {
-		console.log(
-			'Porcentaje de uso de arrow functions del total: ',
-			(Object.keys(results).length / amountOfJs * 100).toFixed(2)
-		);
-	});
+findInFiles.find('=> {', './test', '.js$').then(results => 
+  console.log(
+    'Porcentaje de uso de arrow functions del total: ',
+    (Object.keys(results).length / amountOfJs * 100).toFixed(2)
+  )
+);
 
-	findInFiles.find('([A-Z][a-z]+)+', './test', '.scss$').then(results => {
-		console.log('Cantidad de archivos con cammel case en sass: ', Object.keys(results).length);
-	});
-});
+findInFiles.find('([A-Z][a-z]+)+', './test', '.scss$').then(results => 
+  console.log('Cantidad de archivos con cammel case en sass: ', Object.keys(results).length)
+);
