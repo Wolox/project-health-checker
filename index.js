305e29508fd1083ebdc1c6627a0ee78a0deaafea
MMarussi <jmmarussi32@gmail.com>
2019-06-28 17:29:40 -0300

    Added bootstrap env script check and some changes

--- index.js
+++ index.js
@@ -1,31 +1,28 @@
 require('dotenv').config();
-const findInFiles = require('find-in-files');
+const { find } = require('find-in-files');
+
+const { analyzeMatches } = require('./utils/utils')
 const { requestChangesPercentage } = require('./gitChecks/services/requestChangesPercentaje');
+const { runEnvChecks } = require('./envChecks/envCheck')
 
 let amountOfJs;
 
-findInFiles.find('', './test', '.js$').then(results =>
-	amountOfJs = Object.keys(results).length
+find('', './test', '.js$').then(results =>
+  amountOfJs = Object.keys(results).length
 );
 
-findInFiles.find("from 'i18next*';", './test', '.js$').then(results =>
-	console.log(
-		'Porcentaje de uso de internacionalización del total: ',
-		(Object.keys(results).length / amountOfJs * 100).toFixed(2)
-	)
-);
-
-findInFiles.find('=> {', './test', '.js$').then(results =>
+find("from 'i18next*';", './test', '.js$').then(results =>
   console.log(
-    'Porcentaje de uso de arrow functions del total: ',
-    (Object.keys(results).length / amountOfJs * 100).toFixed(2)
+    `Porcentaje de uso de internacionalización del total: ${(analyzeMatches(results).length / amountOfJs * 100).toFixed(2)}%`
   )
 );
 
-findInFiles.find('([A-Z][a-z]+)+', './test', '.scss$').then(results =>
-  console.log('Cantidad de archivos con cammel case en sass: ', Object.keys(results).length)
+find('((=> {)|(\) =>)', './test', '.js$').then(results =>
+  console.log(
+    `Porcentaje de uso de arrow functions del total: ${(analyzeMatches(results).length / amountOfJs * 100).toFixed(2)}%`
+  )
 );
 
-findInFiles.find('process\.env\.', './test', '.js$').then(results =>
-  console.log('Se necesita .env para correr el proyecto: ', !!Object.keys(results).length)
+find('([A-Z][a-z]+)+', './test', '.scss$').then(results =>
+  console.log('Cantidad de archivos con cammel case en sass: ', Object.keys(results).length)
 );
