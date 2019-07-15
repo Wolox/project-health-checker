6bc8d25c83a3ac44259a3870adb18e7bb597f1c3
lucaszibell <lucas.zibell@wolox.com.ar>
2019-07-15 09:47:12 -0300

    added cheks

--- utils/utils.js
+++ utils/utils.js
@@ -1,6 +1,14 @@
-const removeComments = match => 
-  match.line.filter(line => line.substring(0,2) !== '//')
+const colors = require('../constants/colors');
 
-module.exports.analyzeMatches = 
-  matches => Object.keys(matches).filter(key => removeComments(matches[key]).length)
+const percentage = 100;
 
+const removeComments = match => match.line.filter(line => line.substring(0, 2) !== '//');
+
+module.exports.analyzeMatches = matches =>
+  Object.keys(matches).filter(key => removeComments(matches[key]).length);
+
+module.exports.resolveColor = (value, expected) => (value >= expected ? colors.green : colors.red);
+
+module.exports.calculatePercentage = (results, total) =>
+  // eslint-disable-next-line prettier/prettier
+  (this.analyzeMatches(results).length / total * percentage).toFixed(2);
