3ea55a027df600ba8a0749f46b3a7f3ef85f4ea3
lucaszibell <lucas.zibell@wolox.com.ar>
2019-07-25 18:53:59 -0300

    added line limit

--- constants/limits.js
+++ constants/limits.js
@@ -2,5 +2,7 @@ module.exports = {
   actions: 60,
   i18n: 50,
   arrowFunctions: 60,
-  scss: 15
+  scss: 15,
+  codeOwners: 2,
+  lines: 150
 };
--- index.js
+++ index.js
@@ -2,23 +2,35 @@
 require('dotenv').config();
 const { find, findSync } = require('find-in-files');
 const fs = require('fs');
+const read = require('read-file');
+const parseArgs = require('minimist');
 
 const { resolveColor, calculatePercentage } = require('./utils/utils');
 const colors = require('./constants/colors');
 const limits = require('./constants/limits');
 const { runEnvChecks } = require('./envChecks/envCheck');
 
+const args = parseArgs(process.argv);
+
+let testPath = './test';
+
+if (args.path) {
+  testPath = args.path;
+} else if (args.p) {
+  testPath = args.p;
+}
+
 let amountOfJs = 0;
 let amountOfActionJs = 0;
 let amountOfScss = 0;
 
-runEnvChecks();
+runEnvChecks(testPath);
 
-findSync('', './test', '.js$').then(results => (amountOfJs = Object.keys(results).length));
-findSync('', './test', 'actions.js$').then(results => (amountOfActionJs = Object.keys(results).length));
-findSync('', './test', 'styles.scss$').then(results => (amountOfScss = Object.keys(results).length));
+findSync('', testPath, '.js$').then(results => (amountOfJs = Object.keys(results).length));
+findSync('', testPath, 'actions.js$').then(results => (amountOfActionJs = Object.keys(results).length));
+findSync('', testPath, 'styles.scss$').then(results => (amountOfScss = Object.keys(results).length));
 
-find("from 'i18next*';", './test', '.js$').then(results => {
+find("from 'i18next*';", testPath, '.js$').then(results => {
   const result = calculatePercentage(results, amountOfJs);
   console.log(
     resolveColor(result, limits.i18n),
@@ -26,7 +38,7 @@ find("from 'i18next*';", './test', '.js$').then(results => {
   );
 });
 
-find('=>', './test', '.js$').then(results => {
+find('=>', testPath, '.js$').then(results => {
   const result = calculatePercentage(results, amountOfJs);
   console.log(
     resolveColor(result, limits.arrowFunctions),
@@ -34,7 +46,7 @@ find('=>', './test', '.js$').then(results => {
   );
 });
 
-find('([A-Z][a-z]+)+', './test', '.scss$').then(results => {
+find('([A-Z][a-z]+)+', testPath, '.scss$').then(results => {
   const result = calculatePercentage(results, amountOfScss);
   console.log(
     resolveColor(limits.scss, result),
@@ -42,12 +54,34 @@ find('([A-Z][a-z]+)+', './test', '.scss$').then(results => {
   );
 });
 
-find("from 'redux-recompose';", './test', 'actions.js$').then(results => {
+find("from 'redux-recompose';", testPath, 'actions.js$').then(results => {
   const result = calculatePercentage(results, amountOfActionJs);
   console.log(resolveColor(result, limits.actions), `Porcentaje de actions con redux-recompose: ${result}%`);
 });
 
-fs.access('./test/README.md', fs.F_OK, err => {
+find(/\n/, testPath, 'layout.js$').then(results => {
+  const filtered = Object.keys(results).filter(elem => results[elem].count > limits.lines);
+  console.log('Cantidad de layouts con mas de 150 lineas:', filtered.length);
+});
+
+find(/\n/, testPath, 'index.js$').then(results => {
+  const filtered = Object.keys(results).filter(elem => results[elem].count > limits.lines);
+  console.log('Cantidad de index con mas de 150 lineas:', filtered.length);
+});
+
+read(`${testPath}/.github/CODEOWNERS`, 'utf8', (err, data) => {
+  if (err) {
+    console.log(colors.red, 'No existe un archivo con code owners');
+    return;
+  }
+  const codeOwners = data.split('@').length - 1;
+  console.log(
+    codeOwners > limits.codeOwners ? colors.green : colors.red,
+    `Cantidad de code owners: ${codeOwners}`
+  );
+});
+
+fs.access(`${testPath}/README.md`, fs.F_OK, err => {
   if (err) {
     console.log(colors.red, 'No existe un readme');
     return;
@@ -55,7 +89,7 @@ fs.access('./test/README.md', fs.F_OK, err => {
   console.error(colors.green, 'Existe un readme');
 });
 
-fs.access('./test/.babelrc', fs.F_OK, err => {
+fs.access(`${testPath}/.babelrc`, fs.F_OK, err => {
   if (err) {
     console.log(colors.red, 'No existe un archivo .babelrc');
     return;
