09d8895a4e06d7a24b5918955861b3e45daa0076
rlajous <rodrigo@lajous.com.ar>
2019-08-13 11:51:01 -0300

    Added csv file report in ceo Reports and fixed typo and added EOL in .gitingnor

--- src/seoChecks/index.js
+++ src/seoChecks/index.js
@@ -4,17 +4,24 @@ const fs = require('fs');
 const open = require('open');
 const moment = require('moment');
 const { red } = require('../constants/colors');
+const ReportGenerator = require('lighthouse/lighthouse-core/report/report-generator');
 
-const reportPath = `./ceoReports/ceoReport-${moment().format()}.html`;
+const reportHtmlPath = `./ceoReports/ceoReport-${moment().format()}.html`;
+const reportCsvPath = `./ceoReports/ceoReport-${moment().format()}.csv`;
 
 function launchChromeAndRunLighthouse(url, opts, config = null) {
   return chromeLauncher.launch({ chromeFlags: opts.chromeFlags }).then(chrome => {
     opts.port = chrome.port;
     return lighthouse(url, opts, config).then(results => {
       const res = results.report;
-
       // workaround to save the report
-      fs.writeFile(reportPath, res, err => {
+      fs.writeFile(reportHtmlPath, res, err => {
+        if (err) {
+          console.log(err);
+        }
+      });
+      const csv = ReportGenerator.generateReport(results.lhr, 'csv');
+      fs.writeFile(reportCsvPath, csv, err => {
         if (err) {
           console.log(err);
         }
@@ -25,7 +32,7 @@ function launchChromeAndRunLighthouse(url, opts, config = null) {
 }
 
 const opts = {
-  output: 'html',
+  output: ['html', 'csv'],
   chromeFlags: ['--headless'],
   view: true
 };
@@ -34,9 +41,9 @@ module.exports = seoLink => {
   if (typeof seoLink === 'string') {
     return launchChromeAndRunLighthouse(seoLink, opts).then(() => {
       (async () => {
-        await open(reportPath);
+        await open(reportHtmlPath);
       })();
     });
   }
-  return console.log(red, 'No se paso una url para checkear el ceo');
+  return console.log(red, 'No se paso una url para checkear el seo');
 };
