56125c0d59092954f9e1eb3745ec4d65f87bfb71
martinmona <martin.monastirsky@wolox.com.ar>
2019-09-26 12:05:52 -0300

    update console log message

--- src/unitTests/index.js
+++ src/unitTests/index.js
@@ -1,6 +1,5 @@
 const jest = require("jest");
-
-
+const { red, green } = require('../constants/colors');
 
 module.exports = testPath => {
   
@@ -12,10 +11,14 @@ module.exports = testPath => {
   jest
   .runCLI(options, options.projects)
   .then( (response) => {
-    console.log(response);
+    const averageCoverageTests = Math.round((response.results.numPassedTests / response.results.numTotalTests) * 100)
+    if(averageCoverageTests>=70){
+      console.log(green, "El porcentaje de tests unitarios pasados con éxito es de: " + averageCoverageTests + "%");
+    } else {
+      console.error(red, "El porcentaje de tests unitarios exitosos no supera el 70%, total: " + averageCoverageTests + "%");
+    }
   })
   .catch(() => {
-    console.error("test fallido");
+    console.error("Error al realizar la prueba de covertura");
   });
-
 }
