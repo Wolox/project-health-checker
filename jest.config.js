c8594177230f1bc2c1cb536a00c47df3b80eb98f
martinmona <martin.m94@hotmail.com>
2019-10-18 16:15:59 -0300

    run test with spawn

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,12 @@
 module.exports = {
-  verbose: true,
-  modulePathIgnorePatterns: ['<rootDir>/test/']
+  preset: 'react-native',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  transformIgnorePatterns: [
+    "node_modules/(?!(react-native"
+      + "|react-navigation-tabs"
+      + "|react-native-splash-screen"
+      + "|react-native-screens"
+      + "|react-native-reanimated"
+    + ")/)",
+  ]
 };
--- src/unitTests/index.js
+++ src/unitTests/index.js
@@ -1,7 +1,26 @@
-const jest = require("jest");
 const { red, green } = require('../constants/colors');
+var spawn = require('child_process').spawn;
 
 module.exports = testPath => {
+
+
+  
+  
+
+  /* 
+  exec('cd '+testPath+' && npm test --coverage', (err, stdout) => {
+    console.log("entro");
+    if (err) {
+      // node couldn't execute the command
+      //failed(err);
+      console.error(red, "Error al realizar la prueba de cobertura" + err);
+      return;
+    }
+    
+    // the *entire* stdout and stderr (buffered)
+    console.log(`stdout: ${stdout}`);Z
+    console.log(`stderr: ${stderr}`);
+  }); */
   
   const options = {
     projects: [testPath],
@@ -9,14 +28,20 @@ module.exports = testPath => {
     passWithNoTests: true,
     collectCoverage: true,
     collectCoverageFrom: ["**/src/**/?(*.)[jt]s?(x)"],
-    coverageDirectory: './assets'
-    
+    coverageDirectory: './report',
+    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node']
   };
+  
+  var ls = spawn(/^win/.test(process.platform) ? 'npm.cmd' : 'npm', ['test', '-- --coverage'], { stdio: 'inherit', cwd: testPath} );
 
-  jest
+  
+  ls.on('exit', function (code) {
+    console.log('child process exited with code ' + code.toString());
+  });
+  /* jest
   .runCLI(options, options.projects)
   .then(success)
-  .catch(failed)
+  .catch(failed) */
 
   function success (response){
     if (response.results.numTotalTests == 0){
@@ -32,6 +57,6 @@ module.exports = testPath => {
   }
 
   function failed (response){
-    console.error(red, "Error al realizar la prueba de covertura");
+    console.error(red, "Error al realizar la prueba de covertura" + response);
   }
 }
