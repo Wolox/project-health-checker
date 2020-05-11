493531f63dbf01dadcf29475a50bcaf76eb9df5c
Juan Camilo Morales <camov1209@gmail.com>
2020-05-11 12:00:58 -0500

    remove unused file

--- src/utils/linterFormatter.js
+++
@@ -1,5 +0,0 @@
-module.exports = results => {
-  const errorCount = results.reduce((_errorCount, report) => _errorCount + report.errorCount, 0);
-
-  return `Your code have ${errorCount} ${errorCount === 1 ? 'error' : 'errors'}`;
-};
