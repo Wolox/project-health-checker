b2230eef0d8010564f7bf93fa435428223822bdf
Juan Camilo Morales <camov1209@gmail.com>
2020-05-12 11:57:00 -0500

    enable scoped style validation for every file

--- src/frontEnd/vueChecks/utils.js
+++ src/frontEnd/vueChecks/utils.js
@@ -1,9 +1,7 @@
-const fs = require('fs');
-const path = require('path');
 const { findSync } = require('find-in-files');
 
 /*
-  ⚠️Style gonna be ommited when open tag is multi-line. For example:
+  ⚠️scoped-style validation gonna be ommited when open tag is multi-line. For example:
     <style
       lang="scss"
       scoped
@@ -13,14 +11,6 @@ const { findSync } = require('find-in-files');
 */
 
 module.exports.haveVueFilesScopedStyles = async testPath => {
-  const rootDir = `${testPath}/src`;
-  const rootVueFiles = fs.readdirSync(rootDir).filter(filename => /\.vue$/.test(filename));
-  const styleTagsFound = await findSync('<style', rootDir, '.vue$');
-
-  const filtered = Object.entries(styleTagsFound).filter(([key]) => {
-    const { base: filename } = path.parse(key);
-    return !rootVueFiles.includes(filename);
-  });
-
-  return filtered.every(({ line }) => line[0].includes('scoped'));
+  const styleTagsFound = await findSync('<style', `${testPath}/src`, '.vue$');
+  return Object.values(styleTagsFound).every(({ line }) => line[0].includes('scoped'));
 };
